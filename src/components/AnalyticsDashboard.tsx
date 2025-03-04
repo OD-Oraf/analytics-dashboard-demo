34d899819a5b1b210cf73e0382537e8fb3f78aa6
odoraf <poseidonog@gmail.com>
2025-03-04 15:56:50 -0500

    complete timeseries pageviews

--- src/components/AnalyticsDashboard.tsx
+++ src/components/AnalyticsDashboard.tsx
@@ -1,12 +1,14 @@
 "use client"
-import { Card } from "@tremor/react";
+import {BarChart, Card} from "@tremor/react";
+import {analytics} from "@/utils/analytics";
 
 interface AnalyticsDashboardProps {
     avgVisitorsPerDay: string;
     amtVisitorsToday: number;
+    timeseriesPageviews: Awaited<ReturnType<typeof analytics.retrieveDays>>;
 }
 
-const AnalyticsDashboard = ({ avgVisitorsPerDay, amtVisitorsToday }: AnalyticsDashboardProps) => {
+const AnalyticsDashboard = ({avgVisitorsPerDay, amtVisitorsToday, timeseriesPageviews}: AnalyticsDashboardProps) => {
     return <div className="flex flex-col gap-6">
         <div className="grid w-full mx-auto grid-cols-1 sm:grid-cols-2 gap-6">
             <Card className="w-full mx-auto max-w-xs">
@@ -17,6 +19,24 @@ const AnalyticsDashboard = ({ avgVisitorsPerDay, amtVisitorsToday }: AnalyticsDa
             <Card className="w-full mx-auto max-w-xs">
                 <p className="text-tremor-default text-dark-tremor-content">Visitors Today</p>
                 <p className="text-3xl text-white font-semibold">{amtVisitorsToday}</p>
+            </Card>
+
+            <Card>
+                {timeseriesPageviews ? (
+                    <BarChart
+                        allowDecimals={false}
+                        showAnimation
+                        data = {
+                            timeseriesPageviews.map((day) => ({
+                                name: day.date,
+                                Visitors: day.events.reduce((acc, curr) => {
+                                    return acc + Object.values(curr)[0]!
+                            }, 0),
+                        }))}
+                        categories={["Visitors"]}
+                      index='name'
+                    />
+                ) : null}
             </Card>
         </div>
     </div>
