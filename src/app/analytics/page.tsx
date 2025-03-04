5d110159bf0080552e7cd9ab605a3067c689f73b
odoraf <poseidonog@gmail.com>
2025-03-04 08:36:45 -0500

    update view to retrieve 2 days

--- src/app/analytics/page.tsx
+++ src/app/analytics/page.tsx
@@ -1,7 +1,7 @@
 import { analytics } from "@/utils/analytics";
 
 const Page = async () => {
-  const pageview = await analytics.retrieve("pageview", "11/02/2024")
+  const pageview = await analytics.retrieveDays("pageview", 2)
 
   return <pre className="text-white">{JSON.stringify(pageview)}</pre>;
 };
