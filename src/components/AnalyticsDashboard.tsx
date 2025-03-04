b84e513b62cace003dcaabfad57dd62fb61b9de6
odoraf <poseidonog@gmail.com>
2025-03-04 17:17:43 -0500

    implement weeks top visitors

--- src/components/AnalyticsDashboard.tsx
+++ src/components/AnalyticsDashboard.tsx
@@ -1,14 +1,20 @@
 "use client"
 import {BarChart, Card} from "@tremor/react";
 import {analytics} from "@/utils/analytics";
+import ReactCountryFlag from "react-country-flag";
 
 interface AnalyticsDashboardProps {
     avgVisitorsPerDay: string;
     amtVisitorsToday: number;
     timeseriesPageviews: Awaited<ReturnType<typeof analytics.retrieveDays>>;
+    topCountries: [string, number];
 }
 
-const AnalyticsDashboard = ({avgVisitorsPerDay, amtVisitorsToday, timeseriesPageviews}: AnalyticsDashboardProps) => {
+const Badge = ({percentage}: {percentage: number}) => {
+    const isPositive
+}
+
+const AnalyticsDashboard = ({avgVisitorsPerDay, amtVisitorsToday, timeseriesPageviews, topCountries}: AnalyticsDashboardProps) => {
     return <div className="flex flex-col gap-6">
         <div className="grid w-full mx-auto grid-cols-1 sm:grid-cols-2 gap-6">
             <Card className="w-full mx-auto max-w-xs">
@@ -19,6 +25,26 @@ const AnalyticsDashboard = ({avgVisitorsPerDay, amtVisitorsToday, timeseriesPage
             <Card className="w-full mx-auto max-w-xs">
                 <p className="text-tremor-default text-dark-tremor-content">Visitors Today</p>
                 <p className="text-3xl text-white font-semibold">{amtVisitorsToday}</p>
+            </Card>
+
+            <Card className="flex flex-col sm::grid grid-cols-4 gap-6">
+                <h2 className="w-full text-dark-tremor-content-string text-center
+                sm:left-left font-semibold text-xl">This weeks top visitors</h2>
+                <div className="col-span-3 flex items-center justify-between flex-wrap gap-8">
+                    {topCountries?.map(([countryCode, number]) => {
+                        return <div className="flex items-center gap-3 text-dark-tremor-content-strong">
+                            <p className='hiden sm:block text-tremor-content'>
+                                {countryCode}
+                            </p>
+                            <ReactCountryFlag
+                                className='text-5xl sm:text-3xl'
+                                svg
+                                countryCode={countryCode}
+                            />
+                            <p className="text-tremor-content sm:text-dark-tremor-content-strong">{number}</p>
+                        </div>
+                    })}
+                </div>
             </Card>
 
             <Card>
