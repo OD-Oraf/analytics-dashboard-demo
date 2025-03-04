afc14112106830c25525cc83f7b716ad88346c2f
odoraf <poseidonog@gmail.com>
2025-03-04 15:22:01 -0500

    create extra logic for visitors per day and total visitors today

--- src/app/analytics/page.tsx
+++ src/app/analytics/page.tsx
@@ -1,14 +1,39 @@
 import { analytics } from "@/utils/analytics";
 import AnalyticsDashboard from "@/components/AnalyticsDashboard";
+import { getDate } from "@/utils";
 
 const Page = async () => {
-    const pageview = await analytics.retrieveDays("pageview", 2)
+    const TRACKING_DAYS = 7
 
-    return <div className="min-h-screen w-full py-12 flex justify-center items-center">
-        <div className="relative w-full max-w-6xl mx-auto text-white">
-            <AnalyticsDashboard/>
+    const pageviews = await analytics.retrieveDays("pageview", TRACKING_DAYS)
+
+    const totalPageViews = pageviews.reduce((acc, curr) => {
+        return (
+            acc + curr.events.reduce((acc, curr) => {
+                return acc + Object.values(curr)[0]!
+            }, 0)
+        )
+    }, 0)
+
+
+    const avgVisitorsPerDay = (totalPageViews / TRACKING_DAYS).toFixed(1)
+
+    const amtVisitorsToday = pageviews.filter((ev) => ev.date === getDate()).reduce((acc, curr) => {
+        return (
+            acc + curr.events.reduce((acc, curr) => acc + Object.values(curr)[0]!, 0)
+        )
+    }, 0)
+
+    return (
+        <div className="min-h-screen w-full py-12 flex justify-center items-center">
+            <div className="relative w-full max-w-6xl mx-auto text-white">
+                <AnalyticsDashboard
+                    avgVisitorsPerDay={avgVisitorsPerDay}
+                    amtVisitorsToday={amtVisitorsToday}
+                />
+            </div>
         </div>
-    </div>
+    )
 
 }
 
