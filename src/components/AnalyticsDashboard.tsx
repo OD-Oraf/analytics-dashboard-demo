a0dad44f09c5f719d81744e68a43ad82493b24f7
odoraf <poseidonog@gmail.com>
2025-03-04 15:21:11 -0500

    add additional card with analytics info

--- src/components/AnalyticsDashboard.tsx
+++ src/components/AnalyticsDashboard.tsx
@@ -1,11 +1,22 @@
 "use client"
 import { Card } from "@tremor/react";
 
-const AnalyticsDashboard = () => {
+interface AnalyticsDashboardProps {
+    avgVisitorsPerDay: string;
+    amtVisitorsToday: number;
+}
+
+const AnalyticsDashboard = ({ avgVisitorsPerDay, amtVisitorsToday }: AnalyticsDashboardProps) => {
     return <div className="flex flex-col gap-6">
         <div className="grid w-full mx-auto grid-cols-1 sm:grid-cols-2 gap-6">
             <Card className="w-full mx-auto max-w-xs">
                 <p className="text-tremor-default text-dark-tremor-content">Avg. visitors/day</p>
+                <p className="text-3xl text-white font-semibold">{avgVisitorsPerDay}</p>
+            </Card>
+
+            <Card className="w-full mx-auto max-w-xs">
+                <p className="text-tremor-default text-dark-tremor-content">Visitors Today</p>
+                <p className="text-3xl text-white font-semibold">{amtVisitorsToday}</p>
             </Card>
         </div>
     </div>
