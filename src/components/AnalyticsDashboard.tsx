7a7ea89a7154a474d2c8f1dfbc0589365e4b3cf0
odoraf <poseidonog@gmail.com>
2025-03-04 21:32:12 -0500

    add badge for increasing/decreasing visitors

--- src/components/AnalyticsDashboard.tsx
+++ src/components/AnalyticsDashboard.tsx
@@ -2,6 +2,7 @@
 import {BarChart, Card} from "@tremor/react";
 import {analytics} from "@/utils/analytics";
 import ReactCountryFlag from "react-country-flag";
+import {ArrowDownRight, ArrowRight, ArrowUpRight} from "lucide-react";
 
 interface AnalyticsDashboardProps {
     avgVisitorsPerDay: string;
@@ -10,11 +11,42 @@ interface AnalyticsDashboardProps {
     topCountries: [string, number];
 }
 
-const Badge = ({percentage}: {percentage: number}) => {
-    const isPositive
+const Badge = ({percentage}: { percentage: number }) => {
+    const isPositive = percentage > 0
+    const isNeutral = percentage === 0
+    const isNegative = percentage < 0
+
+    if (isNaN(percentage)) return null
+
+    const positiveClassname = "bg-green-900/25 text-green-400 ring-green-400/25"
+    const neutralClassname = "bg-zinc-900/25 text-zinc-400 ring-zinc-400/25"
+    const negativeClassname = "bg-red-900/25 text-red-400 ring-red-400/25"
+
+    return (
+        <span className={`inline-flex gap-1 items-center rounded-md 
+        px-2 py-1 text-xs fond-medium ring-1 rind-inset ${
+            isPositive 
+            ? positiveClassname 
+            : isNeutral     
+            ? neutralClassname 
+            : negativeClassname
+        }`}>
+            {isPositive ? <ArrowUpRight className="h-3 w-3"/> : null}
+            {isNeutral ? <ArrowRight className="h-3 w-3"/> : null}
+            {isNegative ? <ArrowDownRight className="h-3 w-3"/> : null}
+            {percentage.toFixed(0)}%
+        </span>
+
+    )
+
 }
 
-const AnalyticsDashboard = ({avgVisitorsPerDay, amtVisitorsToday, timeseriesPageviews, topCountries}: AnalyticsDashboardProps) => {
+const AnalyticsDashboard = ({
+                                avgVisitorsPerDay,
+                                amtVisitorsToday,
+                                timeseriesPageviews,
+                                topCountries
+                            }: AnalyticsDashboardProps) => {
     return <div className="flex flex-col gap-6">
         <div className="grid w-full mx-auto grid-cols-1 sm:grid-cols-2 gap-6">
             <Card className="w-full mx-auto max-w-xs">
@@ -23,7 +55,13 @@ const AnalyticsDashboard = ({avgVisitorsPerDay, amtVisitorsToday, timeseriesPage
             </Card>
 
             <Card className="w-full mx-auto max-w-xs">
-                <p className="text-tremor-default text-dark-tremor-content">Visitors Today</p>
+                <p className="flex gap-2.5 items-center text-tremor-default text-dark-tremor-content">
+                    Visitors Today
+                    <Badge percentage={
+                            (amtVisitorsToday / Number(avgVisitorsPerDay) - 1) * 100
+                        }
+                    />
+                </p>
                 <p className="text-3xl text-white font-semibold">{amtVisitorsToday}</p>
             </Card>
 
@@ -52,15 +90,15 @@ const AnalyticsDashboard = ({avgVisitorsPerDay, amtVisitorsToday, timeseriesPage
                     <BarChart
                         allowDecimals={false}
                         showAnimation
-                        data = {
+                        data={
                             timeseriesPageviews.map((day) => ({
                                 name: day.date,
                                 Visitors: day.events.reduce((acc, curr) => {
                                     return acc + Object.values(curr)[0]!
-                            }, 0),
-                        }))}
+                                }, 0),
+                            }))}
                         categories={["Visitors"]}
-                      index='name'
+                        index='name'
                     />
                 ) : null}
             </Card>
